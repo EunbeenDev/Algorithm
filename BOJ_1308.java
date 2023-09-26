39e2117bc3ade164b7e93e3dfe5014eca2055ea9
EunbeenDev <ros4312@naver.com>
2023-09-26 19:54:10 +0900

    [BOJ] 1308 수정

--- BOJ_1308.java
+++ BOJ_1308.java
@@ -1,29 +1,32 @@
 import java.util.*;
+
 public class BOJ_1308 {
 
-    static int[] day(int year){
-        int[] month_days={31,28,31,30,31,30,31,31,30,31,30,31}; //평년 기준
-        if(year%4==0 || year%100!=0 || year%400==0){ month_days[1]=29; }
+    static int[] day(int year) {
+        int[] month_days = { 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 }; // 평년 기준
+        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) { // 윤년 조건 수정
+            month_days[1] = 29;
+        }
         return month_days;
     }
 
-//    1/1/1 ~ y/m/d 총 날짜 수
-    static int sum_days(int y, int m, int d){
-        int result=0;
+    // 1/1/1 ~ y/m/d 총 날짜 수
+    static int sum_days(int y, int m, int d) {
+        int result = 0;
         int[] days;
 
-        for(int i=1; i<y; i++){
-            days=day(i); //위치 선언
-            for(int j=0; j<12; j++){
-                result+=days[j]; //i년 12개월 동안의 날짜수 합
+        for (int i = 1; i < y; i++) {
+            days = day(i); // 위치 선언
+            for (int j = 0; j < 12; j++) {
+                result += days[j]; // i년 12개월 동안의 날짜수 합
             }
         }
 
-        days=day(y);
-        for(int i=0; i<m-1; i++){
-            result+=days[i];
+        days = day(y);
+        for (int i = 0; i < m - 1; i++) {
+            result += days[i];
         }
-        result+=d;
+        result += d;
 
         return result;
     }
@@ -40,13 +43,14 @@ public class BOJ_1308 {
         int d_d = sc.nextInt();
 
         // is gg
-        if ((d_y - today_y > 1000) || (d_y - today_y == today_m && d_d >= today_d))
+        if ((d_y - today_y > 1000) ||
+                (d_y - today_y == 1000 && d_m > today_m) ||
+                (d_y - today_y == 1000 && d_m == today_m && d_d >= today_d)) {
             System.out.println("gg");
-        else {
-            long t = sum_days(today_y, today_m, today_d);
-            long d = sum_days(d_y, d_m, d_d);
+        } else {
+            int t = sum_days(today_y, today_m, today_d);
+            int d = sum_days(d_y, d_m, d_d);
             System.out.println("D-" + (d - t));
         }
-
     }
 }
