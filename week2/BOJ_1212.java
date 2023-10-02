a1c7a81377b08d12a871da55422cf08cae86b003
EunbeenDev <ros4312@naver.com>
2023-10-03 02:34:30 +0900

    [BOJ] 1212 Complete

---
+++ week2/BOJ_1212.java
@@ -0,0 +1,45 @@
+package week2;
+//Bronze_8진수 2진수
+import java.io.*;
+
+public class BOJ_1212 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String S = br.readLine();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i = 0; i < S.length(); i++) {
+            int N = S.charAt(i) - '0';
+            if(N >= 4) {
+                sb.append(1);
+                N -= 4;
+            }else {
+                sb.append(0);
+            }
+            if(N >= 2) {
+                sb.append(1);
+                N -= 2;
+            }else {
+                sb.append(0);
+            }
+            if(N == 1) {
+                sb.append(1);
+                N -= 1;
+            }else{
+                sb.append(0);
+            }
+        }
+
+        if(S.charAt(0) == '1') {
+            sb.delete(0, 2);
+        }else if(S.charAt(0) == '2' || S.charAt(0) == '3') {
+            sb.deleteCharAt(0);
+        }else if(S.charAt(0) == '0') {
+            sb.delete(0, 2);
+        }
+        System.out.println(sb);
+    }
+}
