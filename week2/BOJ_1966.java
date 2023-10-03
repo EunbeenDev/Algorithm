d112bbc22304c47c688ab34a656170c78bb9dff3
EunbeenDev <ros4312@naver.com>
2023-10-04 02:58:34 +0900

    [BOJ] 1966 Complete

---
+++ week2/BOJ_1966.java
@@ -0,0 +1,38 @@
+package week2;
+//Silver_프린터큐
+import java.util.*;
+public class BOJ_1966 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = Integer.parseInt(String.valueOf(sc.nextLine()));
+
+        for (int i = 0; i < T; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int cnt = 0;
+            Queue<int[]> q = new LinkedList<>();
+            for (int j = 0; j < a; j++) {
+                q.add(new int[]{j, sc.nextInt()});
+            }
+
+            while (true) {
+                int[] present = q.remove();
+                boolean tf = true;
+
+                for (int[] queue : q) {
+                    if (queue[1] > present[1]) {
+                        tf = false;
+                        break;
+                    }
+                }
+
+                if (tf) {
+                    cnt++;
+                    if (present[0] == b){break;}
+                } else {q.add(present);}
+            }
+            System.out.println(cnt);
+        }
+    }
+}
