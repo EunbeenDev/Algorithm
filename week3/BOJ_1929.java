83740cf787b2c24aa3692f185ac1157ec1dfb52e
EunbeenDev <ros4312@naver.com>
2023-10-09 17:32:48 +0900

    [BOJ] 1929 Complete
    with Sieve of Eratosthenes

--- week3/BOJ_1929.java
+++ week3/BOJ_1929.java
@@ -1,19 +1,35 @@
 package week3;
 //Silver_소수 구하기
 import java.util.*;
+
 public class BOJ_1929 {
+    public static boolean[] primes;
     public static void main(String[] args) {
+        long beforeTime = System.currentTimeMillis(); // 코드 실행 전에 시간 받아오기
+
         Scanner sc=new Scanner(System.in);
-        int n1=sc.nextInt();
-        int n2=sc.nextInt();
-        while(n1<=n2){
-            boolean tf=false;
-            for(int i=2; i<n1; i++){
-                if(n1%i==0){tf=true; break;}
-            }
-            if(tf==false){System.out.println(n1);}
-            n1++;
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        primes=new boolean[n+1];
+        eratos();
+
+        for(int i=m; i<=n; i++){
+            //false->소수
+            if(!primes[i]){System.out.println(i);}
         }
 
+        long afterTime = System.currentTimeMillis();
+        long diffTime = afterTime - beforeTime;
+        System.out.println("실행 시간(ms): " + diffTime);
+
+    }
+
+    public static void eratos(){
+        primes[0]=primes[1]=true; //0,1은 소수가 아님.
+
+        for(int i=2; i<=Math.sqrt(primes.length);i++){
+            if(primes[i]){continue;}
+            for(int j=i*i;j< primes.length;j+=i){primes[j]=true;}
+        }
     }
 }
