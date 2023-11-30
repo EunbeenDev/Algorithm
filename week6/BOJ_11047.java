e66e0f7590cf3124ab6cd0e5315109a86e66d5eb
EunbeenDev <ros4312@naver.com>
2023-12-01 03:35:42 +0900

    [BOJ] Complete 11047
    with Greedy Algorithm

---
+++ week6/BOJ_11047.java
@@ -0,0 +1,36 @@
+package week6;
+//Silver_동전 0
+import java.util.*;
+
+public class BOJ_11047 {
+    private static int N;
+    private static int value;
+    private static ArrayList<Integer> arr;
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        StringTokenizer st=new StringTokenizer(sc.nextLine());
+        N=Integer.parseInt(st.nextToken());
+        value=Integer.parseInt(st.nextToken());
+        arr=new ArrayList<>();
+        int coin=0;
+
+        for(int i=0; i<N; i++){
+            int new_value=Integer.parseInt(sc.nextLine());
+            if(new_value>value){break;}
+            else{arr.add(new_value);}
+        }
+
+        int i=arr.size()-1;
+
+        while(value!=0){
+            int share=value/(arr.get(i)); //몫
+            int remain=value%(arr.get(i)); //나머지
+
+            value=remain;
+            coin+=share;
+
+            i--;
+        }
+        System.out.print(coin);
+    }
+}
