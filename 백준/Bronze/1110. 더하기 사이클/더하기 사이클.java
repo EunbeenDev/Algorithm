bbababbcc1c8b10844b3393a9a424c00f2c7be3e
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2024-03-16 17:06:13 +0900

    [Bronze I] Title: 더하기 사이클, Time: 248 ms, Memory: 19220 KB -BaekjoonHub

---
+++ 백준/Bronze/1110. 더하기 사이클/더하기 사이클.java
@@ -0,0 +1,48 @@
+
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        int firstNum = n;
+        int cycle = 0;
+
+        while (true) {
+            String k = undertenPzero(n);
+            String newNum = makeNewNum(k);
+            cycle++;
+            int newNumInt = Integer.parseInt(newNum);
+
+            if (newNumInt == firstNum) {
+                System.out.println(cycle);
+                break;
+            } else {
+                n = newNumInt;
+            }
+        }
+    }
+
+    public static String undertenPzero(int n) {
+        if (n < 10) {
+            return "0" + n;
+        } else {
+            return String.valueOf(n);
+        }
+    }
+
+    public static String rangeK1(int k1) {
+        if (k1 < 10) {
+            return String.valueOf(k1);
+        } else {
+            String k1Str = String.valueOf(k1);
+            return k1Str.substring(k1Str.length() - 1);
+        }
+    }
+
+    public static String makeNewNum(String k) {
+        int k1 = Character.getNumericValue(k.charAt(0)) + Character.getNumericValue(k.charAt(1));
+        String k1Str = rangeK1(k1);
+        return k.charAt(1) + k1Str;
+    }
+}
