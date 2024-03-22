1a7771607347d1534e1e306129ab05edc219a48d
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2024-03-22 13:22:25 +0900

    [Bronze II] Title: 알파벳 찾기, Time: 228 ms, Memory: 18328 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+ 
+public class Main {
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int[] arr = new int[26];
+		
+		for(int i = 0; i < arr.length; i++) {arr[i] = -1;}
+		String S = in.nextLine();
+ 
+		for(int i = 0; i < S.length(); i++) {
+			char ch = S.charAt(i);
+    
+			if(arr[ch - 'a'] == -1) {	// arr 원소 값이 -1 -> 초기화
+				arr[ch - 'a'] = i;
+			}
+		}
+ 
+		for(int val : arr) {
+			System.out.print(val + " ");
+		}
+	}
+}
