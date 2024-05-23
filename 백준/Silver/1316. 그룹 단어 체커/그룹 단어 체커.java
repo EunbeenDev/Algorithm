1c0517ee8c7603b4e51eadaf34c22606715b14e0
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2024-05-24 02:01:42 +0900

    [Silver V] Title: 그룹 단어 체커, Time: 212 ms, Memory: 17732 KB -BaekjoonHub

---
+++ 백준/Silver/1316. 그룹 단어 체커/그룹 단어 체커.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Main {
+
+    private static boolean isGroup(String str) {
+        Set<Character> set = new HashSet<>();
+        char prevChar = 0; //변수 초기화
+
+        for (int i = 0; i < str.length(); i++) {
+            char currentChar = str.charAt(i);
+
+            if (prevChar != currentChar) {
+                if (set.contains(currentChar)) {
+                    return false; // 현재 문자가 이미 나왔던 문자-> 그룹 단어 X
+                }
+                set.add(currentChar);
+            }
+            prevChar = currentChar;
+        }
+
+        return true; //그룹 단어 O
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int cnt = 0;
+
+        for (int t = 0; t < n; t++) {
+            String str = sc.next();
+            if (isGroup(str)) { cnt++; }
+        }
+        System.out.println(cnt);
+    }
+}
