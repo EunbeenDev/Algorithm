b3a60955809d5a65b0ce20bb2a88f4fe8a871ed1
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2024-06-18 20:53:39 +0900

    [Silver IV] Title: DNA, Time: 168 ms, Memory: 17144 KB -BaekjoonHub

---
+++ 백준/Silver/1969. DNA/DNA.java
@@ -0,0 +1,77 @@
+import java.io.*;
+
+public class Main {
+	static int N, M;
+	static String[] DNA;
+	static int hd;
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+		String[] sarr = br.readLine().split(" ");
+		N = Integer.parseInt(sarr[0]);
+		M = Integer.parseInt(sarr[1]);
+
+		DNA = new String[N];
+
+		for (int i = 0; i < N; i++) {
+			DNA[i] = br.readLine();
+		}
+
+		StringBuffer buf = new StringBuffer();
+
+		for (int i = 0; i < M; i++) {
+			int[] cnt = new int[4]; // A C G T
+			for (int j = 0; j < N; j++) {
+				char ch = DNA[j].charAt(i);
+
+				switch (ch) {
+				case 'A':
+					cnt[0]++;
+					break;
+				case 'C':
+					cnt[1]++;
+					break;
+				case 'G':
+					cnt[2]++;
+					break;
+				case 'T':
+					cnt[3]++;
+					break;
+				}
+
+			}
+
+			int idx = 0;
+			int max = 0;
+			for (int k = 0; k < 4; k++) {
+				if (cnt[k] > max || (cnt[k] == max && k < idx)) {
+					max = cnt[k];
+					idx = k;
+				}
+			}
+			switch (idx) {
+			case 0:
+				buf.append('A');
+				break;
+			case 1:
+				buf.append('C');
+				break;
+			case 2:
+				buf.append('G');
+				break;
+			case 3:
+				buf.append('T');
+				break;
+			}
+			hd += N - max;
+
+		}
+		bw.write(buf.toString() + "\n");
+		bw.write(hd + "\n");
+		bw.flush();
+	}
+
+}
