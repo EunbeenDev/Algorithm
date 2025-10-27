448b2d4d24ac99f7b76f3a8dc84d7351100b881c
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2025-10-27 15:40:38 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 1.48 ms, Memory: 97.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.java
@@ -0,0 +1,11 @@
+class Solution {
+    public String solution(int n) {
+        StringBuilder sb = new StringBuilder();
+        for(int i=1;i<=n;i++){
+            if(i%2==0){sb.append("박");}
+            else{sb.append("수");}
+        }
+        
+        return sb.toString();
+    }
+}
