14cd9b8848be99bb44398866331d11106adfa95c
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2026-01-26 15:57:32 +0900

    [Silver IV] Title: 제로, Time: 728 ms, Memory: 181444 KB -BaekjoonHub

--- 백준/Silver/10773. 제로/제로.java
+++ 백준/Silver/10773. 제로/제로.java
@@ -1,18 +1,23 @@
-import java.io.*;
 import java.util.*;
-public class Main {
-    public static void main(String[] args) throws IOException{
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        int T=Integer.parseInt(br.readLine());
-        int sum=0;
-        ArrayList<Integer> nums=new ArrayList();
-        int s=nums.size();
-        for(int i=0; i<T; i++){
-            int num=Integer.parseInt(br.readLine());
-            if(num==0){sum-=nums.get(s-1); nums.remove(s-1); s--;}
-            else{sum+=num; nums.add(num); s++;}
-        }
-        System.out.println(sum);
-    }
 
+public class Main {
+    public static void main(String[] args) {
+        int answer = 0;
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+        for(int i=0; i<n; i++){
+            int num = Integer.parseInt(sc.nextLine());
+            if(num !=0){ dq.addFirst(num);}
+            else{dq.removeFirst();}
+
+        }
+        
+        for(int d : dq){
+            answer+=d;
+        }
+        
+        System.out.println(answer);
+    }
 }
