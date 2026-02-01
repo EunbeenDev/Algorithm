0611bc49388255b18019ba2abc6a9b0ec341d4ac
Eunbeen Noh <101248968+EunbeenDev@users.noreply.github.com>
2026-02-01 15:06:34 +0900

    [Silver IV] Title: 스택, Time: 180 ms, Memory: 20884 KB -BaekjoonHub

--- 백준/Silver/10828. 스택/스택.java
+++ 백준/Silver/10828. 스택/스택.java
@@ -1,87 +1,41 @@
 import java.io.*;
 import java.util.*;
- 
+
 public class Main {
- 
-	public static int[] stack;
-	public static int size = 0;
- 
- 
-	public static void main(String[] args) throws IOException {
- 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		StringTokenizer st;
-		
-		int N = Integer.parseInt(br.readLine());
-		stack = new int[N];
-		
-		while (N-- > 0) {
-			st = new StringTokenizer(br.readLine(), " ");
- 
-			switch (st.nextToken()) {
-			
-			case "push":
-				push(Integer.parseInt(st.nextToken()));
-				break;
-				
-			case "pop":
-				sb.append(pop()).append('\n');
-				break;
-				
-			case "size":
-				sb.append(size()).append('\n');
-				break;
-				
-			case "empty":
-				sb.append(empty()).append('\n');
-				break;
-				
-			case "top":
-				sb.append(top()).append('\n');
-				break;
-			}
-		}
-		System.out.println(sb);
-	}
- 
-	public static void push(int item) {
-		stack[size] = item;
-		size++;
-	}
-	
-	public static int pop() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			int res = stack[size - 1];
-			stack[size - 1] = 0;
-			size--;
-			return res;
-		}
-	}
-	
-	public static int size() {
-		return size;
-	}
-	
-	public static int empty() {
-		if(size == 0) {
-			return 1;
-		}
-		else {
-			return 0;
-		}
-	}
-	
-	public static int top() {
-		if(size == 0) {
-			return -1;
-		}
-		else {
-			return stack[size - 1];
-		}
-	}
-	
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        ArrayDeque<Integer> dq = new ArrayDeque<>();
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0; i<n; i++){
+            String[] line = br.readLine().split(" ");
+            switch(line[0]){
+                case("push"):
+                    dq.addLast(Integer.parseInt(line[1]));
+                    break;
+
+                case("pop"):
+                    if(dq.isEmpty()){sb.append("-1\n");}
+                    else{sb.append(dq.getLast()+"\n");dq.removeLast();}
+                    break;
+
+                case("size"):
+                    sb.append(dq.size()+"\n");
+                    break;
+
+                case("empty"):
+                    if(dq.isEmpty()){sb.append("1\n");}
+                    else{sb.append("0\n");}
+                    break;
+
+                case("top"):
+                    if(dq.isEmpty()){sb.append("-1\n");}
+                    else{sb.append(dq.getLast()+"\n");}
+                    break;
+            }
+        }
+        System.out.println(sb.toString());
+    }
 }
